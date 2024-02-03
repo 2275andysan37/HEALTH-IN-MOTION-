e687d9b4a3397410c2094358490781202de35df3
Bryan M. <brymuma@gmail.com>
2024-02-03 16:03:52 -0600

    Comentarios en el codigo

--- Codigo/test.py
+++ Codigo/test.py
@@ -2,27 +2,34 @@
 from gpiozero import LED, DistanceSensor
 import time
 
+#Sensores base con 3 LED y el sensor de proximidad
 sensor = DistanceSensor(echo = 14, trigger = 15)
 verde = LED(13)
 amarillo = LED(19)
 rojo = LED(26)
+
+#Inicializando los LED's apagados
 verde.off()
 amarillo.off()
 rojo.off()
+
+#Medir la distancia
 while True:
     distance = sensor.distance * 100
     print ("Distancia: ", distance)
-    
+    #Si la distancia es menor a 20cm prender el foco rojo
     if distance < 20:
         amarillo.off()
         verde.off()
         rojo.on()
-    
+        
+    #Si la distancia es mayor a 80, encender el foco verde
     elif distance > 80:
         amarillo.off()
         rojo.off()
         verde.on()
         
+    #Si la distancia es mayor a 20cm y menos a 80cm, encender el foco amarillo
     elif distance > 20 and distance < 80:
         verde.off()
         rojo.off()

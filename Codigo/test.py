2bfa11e2df744bd5a833f4a72d88ab5cb4ce104e
Bryan M. <brymuma@gmail.com>
2024-02-03 16:00:01 -0600

    Codigo base para el script de health in motion

---
+++ Codigo/test.py
@@ -0,0 +1,31 @@
+#Modelo base de Health-In-Motion
+from gpiozero import LED, DistanceSensor
+import time
+
+sensor = DistanceSensor(echo = 14, trigger = 15)
+verde = LED(13)
+amarillo = LED(19)
+rojo = LED(26)
+verde.off()
+amarillo.off()
+rojo.off()
+while True:
+    distance = sensor.distance * 100
+    print ("Distancia: ", distance)
+    
+    if distance < 20:
+        amarillo.off()
+        verde.off()
+        rojo.on()
+    
+    elif distance > 80:
+        amarillo.off()
+        rojo.off()
+        verde.on()
+        
+    elif distance > 20 and distance < 80:
+        verde.off()
+        rojo.off()
+        amarillo.on()
+        
+    time.sleep(0.5)

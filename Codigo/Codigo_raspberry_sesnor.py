52f989ae427288ef74015adfb5a91c95f63de5e0
Bryan M. <brymuma@gmail.com>
2024-02-03 18:50:59 -0600

    Correccion de GPIO's en LED y Sensor

--- Codigo/Codigo_raspberry_sesnor.py
+++ Codigo/Codigo_raspberry_sesnor.py
@@ -3,10 +3,10 @@ from gpiozero import LED, DistanceSensor
 from datetime import datetime
 import time
 
-#Sensores base con 3 LED y el sensor de proximidad
-sensor = DistanceSensor(echo = 14, trigger = 15)
+#Sensores base con 2 LED y el sensor de proximidad
+sensor = DistanceSensor(echo = 21, trigger = 20)
 verde = LED(13)
-amarillo = LED(19)
+amarillo = LED(6)
 
 #Inicializando los LED's apagados
 verde.off()
@@ -17,14 +17,15 @@ while True:
     distance = sensor.distance * 100
     print ("Distancia: ", distance)
     #Si la distancia es menor a 7 prender el foco verde
-    if distance < 7:
+    if distance < 10:
         amarillo.off()
         verde.on()
+        #Tomar la hora en la que acerco la mano e imprimirlo
         checkin = datetime.now()
         print("El sensor recibio el check-in de la mano... a las:", checkin)
                 
-    #Si la distancia es mayor a 7, encender el foco amarillo
-    elif distance > 7:
+    #Si la distancia es mayor a 10, encender el foco amarillo
+    elif distance > 10:
         amarillo.on()
         verde.off()
         

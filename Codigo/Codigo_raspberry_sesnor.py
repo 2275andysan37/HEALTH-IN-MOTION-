760fda1d1ef52265c02d6e958fc13e3f0344da9c
Bryan M. <brymuma@gmail.com>
2024-02-08 22:36:17 -0600

    Keyboard interrupt par apagar los focos encendidos

--- Codigo/Codigo_raspberry_sesnor.py
+++ Codigo/Codigo_raspberry_sesnor.py
@@ -13,20 +13,24 @@ verde.off()
 amarillo.off()
 
 #Medir la distancia
-while True:
-    distance = sensor.distance * 100
-    #Si la distancia es menor a 7 prender el foco verde
-    if distance < 10:
-        amarillo.off()
-        verde.on()
-        #Tomar la hora en la que acerco la mano e imprimirlo
-        checkin = datetime.now()
-        print("El sensor recibio el check-in de la mano... a las:", checkin)
-                
-    #Si la distancia es mayor a 10, encender el foco amarillo
-    elif distance > 10:
-        amarillo.on()
-        verde.off()
-        
-    #Medir la distancia cada medio segundo
-    time.sleep(2)
+try:
+    while True:
+        distance = sensor.distance * 100
+        #Si la distancia es menor a 7 prender el foco verde
+        if distance < 10:
+            amarillo.off()
+            verde.on()
+            #Tomar la hora en la que acerco la mano e imprimirlo
+            checkin = datetime.now()
+            print("El sensor recibio el check-in de la mano... a las:", checkin)
+                    
+        #Si la distancia es mayor a 10, encender el foco amarillo
+        elif distance > 10:
+            amarillo.on()
+            verde.off()
+            
+        #Medir la distancia cada medio segundo
+        time.sleep(1)
+except KeyboardInterrupt:
+    verde.off()
+    amarillo.off()
